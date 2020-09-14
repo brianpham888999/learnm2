86adedb73770d733df7d9a56dd85c9faa1b0bc4d
John McCombs <jmccombs@coredevelop.com>
2020-09-14 09:31:26 +0700

    ExamOne: update using BeforePlugin and AfterPlugin

--- app/code/Core/ExamOne/Plugin/Model/Product.php
+++ app/code/Core/ExamOne/Plugin/Model/Product.php
@@ -5,22 +5,22 @@ namespace Core\ExamOne\Plugin\Model;
 
 class Product
 {
-/*  public function beforeGetFinalPrice(\Magento\Catalog\Model\Product $subject, $result, $qty = null)
+    public function beforeGetFinalPrice(\Magento\Catalog\Model\Product $subject, $qty = null)
     {
-        return $result;
-    }*/
+        $qty = (float)5;
+        return $qty;
+    }
 
-    public function aroundGetFinalPrice(\Magento\Catalog\Model\Product $subject,callable $proceed, $qty = null)
+/*    public function aroundGetFinalPrice(\Magento\Catalog\Model\Product $subject,callable $proceed, $qty = null)
     {
         $qty = (float)5;
         $proceed();
         return (float)100 * $qty;
-    }
-
-
-    /*public function afterGetFinalPrice(\Magento\Catalog\Model\Product $subject, $result, $qty)
-    {
-        if(isset($qty)) $qty = 1;
-        return (float)100 * $qty;
     }*/
+
+
+    public function afterGetFinalPrice(\Magento\Catalog\Model\Product $subject, $result, $qty)
+    {
+        return (float)100 * $qty;
+    }
 }
